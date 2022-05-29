b573c2ab34d239d9a31ed88b28e9cc774aedd96f
Michal Procházka <prochazka2003@gmail.com>
2022-05-29 17:37:33 +0200

    Added documentation

--- main.c
+++ main.c
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,10 +11,20 @@
 #include "MODPlay/modplay.h"
 #include "tune.h"
 
+/*
+ * Rules for picking the right audio pins:
+ * - The left channel pin must have an even number, the right one must have an odd number.
+ * - They must be consecutive.
+ * 
+ * Valid combinations are: 0 and 1, 2 and 3, ..., 8 and 9... you get the idea.
+ */
+
 #define AUDIO_L 8
 #define AUDIO_R 9
 
-#define BUFFERSIZE 16384 // 512 samples per channel
+#define AUDIO_SLICE ((AUDIO_L >> 1u) & 7u)
+
+#define BUFFERSIZE 16384 // 8192 samples per channel
 
 uint16_t samples[BUFFERSIZE * 2]; // double-buffered
 
@@ -28,7 +32,11 @@ volatile uint32_t rendered = 0, played = 0;
 
 void second_core() {
 	while(1) {
+		// Wait until the target buffer becomes outdated
+
 		while(rendered == (played & BUFFERSIZE));
+
+		// Render new audio!
 
 		gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
@@ -36,47 +44,54 @@ void second_core() {
 
 		gpio_put(PICO_DEFAULT_LED_PIN, 0);
 
+		// Switch the buffers
+
 		rendered ^= BUFFERSIZE;
 	}
 }
 
 void pwm_interrupt_handler() {
-	uint8_t slice = pwm_gpio_to_slice_num(AUDIO_L);
+	// Acknowledge the interrupt and output data from the audio buffer
 
-	pwm_clear_irq(slice);
+	pwm_clear_irq(AUDIO_SLICE);
 
-	pwm_set_chan_level(slice, PWM_CHAN_A, (samples[played++] ^ 0x8000) >> 4);
-	pwm_set_chan_level(slice, PWM_CHAN_B, (samples[played++] ^ 0x8000) >> 4);
+	pwm_set_chan_level(AUDIO_SLICE, PWM_CHAN_A, (samples[played++] ^ 0x8000) >> 4);
+	pwm_set_chan_level(AUDIO_SLICE, PWM_CHAN_B, (samples[played++] ^ 0x8000) >> 4);
+
+	// Limit the audio buffer pointer, so it doesn't run off anywhere
 
 	played &= (BUFFERSIZE * 2 - 1);
 }
 
 int main() {
+	// Initialize the library
+
 	InitMOD(tune, 44100);
+
+	// Initialize the internal LED
 
 	gpio_init(PICO_DEFAULT_LED_PIN);
 	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
+	// Initialize PWM and set up the interrupt @ 44.1 kHz
+
 	gpio_set_function(AUDIO_L, GPIO_FUNC_PWM);
 	gpio_set_function(AUDIO_R, GPIO_FUNC_PWM);
 
-	uint slice_num = pwm_gpio_to_slice_num(AUDIO_L);
+    pwm_clear_irq(AUDIO_SLICE);
+    pwm_set_irq_enabled(AUDIO_SLICE, true);
 
-    pwm_clear_irq(slice_num);
-    pwm_set_irq_enabled(slice_num, true);
-    // set the handle function above
     irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler); 
     irq_set_enabled(PWM_IRQ_WRAP, true);
 
-	// Attempt 44.1 kHz
-	pwm_set_wrap(slice_num, clock_get_hz(clk_sys) / 44100);
+	pwm_set_wrap(AUDIO_SLICE, clock_get_hz(clk_sys) / 44100);
+	pwm_set_enabled(AUDIO_SLICE, true);
 
-	pwm_set_chan_level(slice_num, PWM_CHAN_A, 2048);
-	pwm_set_chan_level(slice_num, PWM_CHAN_B, 2048);
-
-	pwm_set_enabled(slice_num, true);
+	// Start the player!
 
 	multicore_launch_core1(second_core);
+
+	// Ideally, we should do something useful here, but this is just a demo, so who cares
 
 	while(1);
 }
